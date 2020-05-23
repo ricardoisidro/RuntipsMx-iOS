6ae1b84130e7d728e6439a36bfea82edbd3f781c
ricardo <ricardo.isidro.86@gmail.com>
2020-05-23 17:18:38 -0500

    Initial Commit

---
+++ Runtips-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Runtips-iOS
+//
+//  Created by Ricardo Isidro on 5/23/20.
+//  Copyright © 2020 Ricardo Isidro Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
